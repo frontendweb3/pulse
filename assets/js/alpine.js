fbae72b3d4105643949ccf1f442f66ed34553ed5
Rajdeep Singh <radewala.2@gmail.com>
2026-08-30 14:17:29 +0530

    Add the collapse plugin on alpine.js

--- assets/js/alpine.js
+++ assets/js/alpine.js
@@ -1,7 +1,11 @@
 // Initialize Alpine.js
 import Alpine from 'alpinejs';
+import collapse from '@alpinejs/collapse';
 
 export function initAlpine() {
+  // Register plugins
+  Alpine.plugin(collapse);
+
   // Make Alpine available globally for use in HTML
   window.Alpine = Alpine;
   
