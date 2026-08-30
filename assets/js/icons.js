4e00e9d21bb5fb63976db336510af748bec8cf12
Rajdeep Singh <radewala.2@gmail.com>
2026-08-30 14:16:58 +0530

    update/add new icon

--- assets/js/icons.js
+++ assets/js/icons.js
@@ -1,16 +1,23 @@
-// Initialize Lucide icons - include only the icons you need
-import { createIcons, Sun, Moon, Search,UserRound, SendHorizontal, Share2, Globe } from 'lucide';
+// Initialize Lucide icons
+import { createIcons, Search, Share2, Globe, Grid2x2, Grid3x3, LayoutGrid, Check, Copy, Link, ArrowRight, ChevronRight, UserRound, Rss, MessageSquare } from 'lucide';
 
 export function initIcons() {
   createIcons({
     icons: {
-      Sun,
-      Moon,
       Search,
-      UserRound,
-      SendHorizontal,
       Share2,
-      Globe
+      Globe,
+      Grid2x2,
+      Grid3x3,
+      LayoutGrid,
+      Check,
+      Copy,
+      Link,
+      ArrowRight,
+      ChevronRight,
+      UserRound,
+      Rss,
+      MessageSquare
     }
   });
 }
