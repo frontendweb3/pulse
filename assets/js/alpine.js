5d3d7a08fba0095940a8adaeb3d22c6c1fca246f
Rajdeep Singh <radewala.2@gmail.com>
2026-09-10 20:29:15 +0530

    feat: add @alpinejs/intersect and implement intersection-based entrance animations for post sidebars

--- assets/js/alpine.js
+++ assets/js/alpine.js
@@ -1,10 +1,12 @@
 // Initialize Alpine.js
 import Alpine from 'alpinejs';
 import collapse from '@alpinejs/collapse';
+import intersect from '@alpinejs/intersect';
 
 export function initAlpine() {
   // Register plugins
   Alpine.plugin(collapse);
+  Alpine.plugin(intersect);
 
   // Make Alpine available globally for use in HTML
   window.Alpine = Alpine;
